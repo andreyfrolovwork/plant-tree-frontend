23ee5392532277a81b316a2d01b8c0872574ff42
alex orlov <iobox420@gmail.com>
2022-07-26 16:55:25 +0500

    init

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,9 +3,6 @@ import { useForm } from '../hooks/useForm'
 import { useAuth } from '../lib/auth'
 import TgButton from './TgButton.jsx'
 import { Button } from 'antd'
-import Cookies from 'js-cookie'
-
-Cookies.set('foo', 'bar')
 
 export function Login() {
   const { login } = useAuth()
@@ -16,7 +13,7 @@ export function Login() {
     hash: "7d282c9fae0b20524c63638946dfcbda94b66f29983918d6d1cc72a234e35a1c"
     id: 1154486226
     photo_url: "https://t.me/i/userpic/320/jPHN0HQDqvnUMUvy2gsZirOpaxyuQRrpbFki9RAKjQg.jpg"
-    username: "iobox420"*/
+    username: "iobox420" */
   const cbOnAuth = user => {
     console.log('Logged in as ' + user)
     logWithTelegram({
@@ -28,11 +25,7 @@ export function Login() {
       username: 'iobox420',
     })
   }
-  const getcookieHandle = () => {
-    console.log(Cookies.get())
-    console.log(Cookies.get('foo'))
-    debugger
-  }
+
   return (
     <div>
       Login
@@ -52,7 +45,6 @@ export function Login() {
         <input type="password" placeholder="password" name="password" onChange={onChange} />
         <button type="submit">Submit</button>
         <TgButton botName={'plant_trees_worldwide_bot'} callbackOnAuth={cbOnAuth} />
-        <Button onClick={getcookieHandle}>get cookie</Button>
       </form>
       {error && <div style={{ color: 'tomato' }}>{JSON.stringify(error, null, 2)}</div>}
     </div>
